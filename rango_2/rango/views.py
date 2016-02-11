f15bbfa37abcefae3742c648a8acd9447fb29e7c
John Angel <usrnja@localhost.localdomain>
2016-02-10 21:06:47 -0600

    Added 2 pages to app

--- rango_2/rango/views.py
+++ rango_2/rango/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Rango says... hello world<br><a href='rango/about/'>about</a>")
+
+
+def about(request):
+    return HttpResponse('Rango says... this is the about page.<br><a href=\'/rango/\'>Home</a>')
